2ff3fd9e76ba6d3c769b3bacd65a3f59c221ff87
Maria Obedkova <Maria Obedkova>
2016-04-25 00:45:07 +0300

    add some files

--- pos-tagger/amh_rules.py
+++ pos-tagger/amh_rules.py
@@ -51,6 +51,10 @@ num = codecs.open('numerals.txt', 'r', 'utf-8')
 numerals = [line.strip() for line in num]
 num.close()
 
+q_prn = codecs.open('quest_pronouns.txt', 'r', 'utf-8')
+quest_pronouns = [line.strip() for line in q_prn]
+q_prn.close()
+
 ### MORPHOLOGY
 
 # check plural for nouns
@@ -83,11 +87,11 @@ if word >= 4:
 # check possessive suffix
 if word >= 2:
     if word[-1] in vowel_e or word[-1] in vowel_u or word[-1] == u'ው':
-        print '1 or 3mask DETECTED'
+        print 'NOUN: 1 or 3mask DETECTED'
     if word[-1] == u'ህ' or word[-1] == u'ህ' or word[-1] == u'ዎ' or word[-1] == u'ዋ':
-        print '2 or 3fem DETECTED'
+        print 'NOUN: 2 or 3fem DETECTED'
     if word[-2:] == u'ቸው' or word[-2:] == u'ችን' or word[-2:] == u'ቸሁ':
-        print '3form and plural DETECTED'
+        print 'NOUN: 3formal and plural DETECTED'
     # todo мб понадобится знать про предыдущую букву (на листке)
     # после себя может присоединять множ число
     # могут присоединяться к местоим, числит, сущ
@@ -95,7 +99,7 @@ if word >= 2:
 # check demonstartives
 if word in demonstratives:
     print 'DEMONSTRATIVE DETECTED'
-if word[:2] == u'የዚ':
+if word[:2] == u'የዚ' or word[:2] == u'በዚ' or word[:2] == u'ከዚ':
     try_word = word[2:]
     if try_word in demonstratives:
         print 'DEMONSTRATIVE DETECTED'
@@ -123,7 +127,105 @@ if word in numerals:
     print 'CARDINAL DETECTED'
 if word[-1] == u'ኛ' or word[-1] == u'ም':
     print 'ORDINAL DETECTED'
-    # todo если есть еще такое окончание, то придется делать поиск по numerals
+    # todo если есть еще такое окончание, то придется делать поиск по numerals ПРИДЕТСЯ!
+
+# check particles
+if word == u'ወይ' or word[-1] == u'ን':
+    print 'PARTICLE DETECTED'
+    # ን приклеивается к слову
+    # todo мб ን убрать надо
+
+# check verbal past
+if word[-2] in consonants and (word[-1] == u'ህ' or word[-1] == u'ክ' or word[-1] == u'ሁ'or word[-1] == u'ኩ' or word[-1] == u'ሽ'):
+    print 'VERB: 2infromal or 1pl DETECTED'
+if word[-1] in vowel_u:
+    print 'VERB: 2formal or 3formal DETECTED'
+if word[-1] in vowel_ae:
+    print 'VERB: 3mask DETECTED'
+if word[-1] in vowel_ae and word[-1] == u'ች':
+    print 'VERB: 3fem DETECTED'
+if word[-1] == u'አችሀ':
+    print 'VERB: 1sg or 2pl DETECTED'
+
+# check accusative
+if word[-1] == u'ን':
+    print 'ACCUSATIVE DETECTED'
+
+# check prepositions
+if word[0] == u'በ' or word[0] == u'ባ' or word[0] == u'ከ' or word[0] == u'ካ' or word[0] == u'እ' or word[0] == u'ለ' or word[0] == u'ስ':
+    print 'PREPOSITION DETECTED'
+
+# check object pronoun suffix
+object_suffixes = [u'ኝ', u'ህ', u'ሽ', u'ው', u'ን']
+if word[-1] in object_suffixes:
+    print 'OBJECT SUFFIX DETECTED'
+if word[-2:] == u'ዎት':
+    print 'OBJECT SUFFIX DETECTED'
+if word[-2] in vowel_a and word[-1] == u'ት':
+    print 'OBJECT SUFFIX DETECTED'
+if word[-3] in vowel_a and (word[-2:] == u'ቸው' or word[-2:] == u'ችሁ' or word[-2:] == u'ቸው' or word[-2:] == u'ችሁ'):
+    print 'OBJECT SUFFIX DETECTED'
+if word[-1] == u'ት' and word[-2] in vowel_u:
+    print 'OBJECT SUFFIX DETECTED'
+    # значит это глагол
+
+# check object pronoun suffix and negation:
+if word[-1] == u'ም':
+    print 'NEGATION DETECTED'
+    if word[-2] in object_suffixes:
+        print 'OBJECT SUFFIX DETECTED'
+    if word[-3:] == u'ዎት':
+        print 'OBJECT SUFFIX DETECTED'
+    if word[-3] in vowel_a and word[-2] == u'ት':
+        print 'OBJECT SUFFIX DETECTED'
+    if word[-4] in vowel_a and (word[-3:] == u'ቸው' or word[-3:] == u'ችሁ' or word[-3:] == u'ቸው' or word[-3:] == u'ችሁ'):
+        print 'OBJECT SUFFIX DETECTED'
+    if word[-2] == u'ት' and word[-3] in vowel_u:
+        print 'OBJECT SUFFIX DETECTED'
+    # значит это глагол
+
+# check negation
+if word[-1] == u'ም' and word[0] == u'አ':
+    print 'NEGATION DETECTED'
+    # verbal negation
+
+# check prefix of verbal present-future stem
+if word[0] == u'እ' or word[0] == u'ች' or word[0] == u'ይ':
+    print 'PRESENT-FUTURE PREFIX DETECTED'
+    # обычно начинаются глаголы на согл, есть исключения, но их мало. поэтому только согласные проверяем
+
+# check verbal present-future form
+if word[-3] in vowel_a and (word[-2:] == u'ለሁ' or word[-2:] == u'ለህ' or word[-2:] == u'ለሽ' or word[-2:] == u'ለች' or word[-2:] == u'ለን' or word[-2:] == u'ችሁ'):
+    print 'PRESENT-FUTURE FORM DETECTED'
+if word[-2] in vowel_a and (word[-1] == u'ሉ' or word[-1] == u'ል'):
+    print 'PRESENT-FUTURE FORM DETECTED'
+    # суффикс - это вспомогательный глагол
+    # todo объектный суффикс после глагольного суффикса
+
+# check question pronoun
+if word in quest_pronouns:
+    print 'QUESTION PRONOUN DETECTED'
+if (word[-2] == u'ኛ' and (word[-1] == u'ው' or word[-1] == u'ዋ' or word[-1] in vowel_u)) or (word[-1] == u'ም' and (word[-2] == u'ው' or word[-2] == u'ዋ' or word[-2] in vowel_u)):
+    try_word = word[:-2]
+    if try_word in quest_pronouns:
+        print 'QUESTION PRONOUN DETECTED'
+
+# todo проставить ограничения по длине
+
+# check infinitive
+if word[0] == u'መ':
+    print 'INFINITIVE PREFIX DETECTED'
+if word[-1] == u'ት':
+    print 'INFINITIVE SUFFIX DETECTED'
+if word[:3] == u'አለመ':
+    print 'INFINITIVE PREFIX WITH NEGATION DETECTED'
+
+# check passive
+if word[0] == u'ተ':
+    print 'PASSIVE DETECTED'
+
+
+
 
 
 
